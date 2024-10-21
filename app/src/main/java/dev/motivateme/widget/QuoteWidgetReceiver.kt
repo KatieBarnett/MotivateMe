d3a1cded0fa1e74703395eca468ca10b7979715c
Katie Barnett <katie.barnett@bilue.com.au>
2024-10-22 08:48:27 +1100

    Workshop: Activity 2, Task 4: Create a simple `GlanceAppWidgetReceiver` that just has an override for `glanceAppWidget`, set it to your `QuoteWidget`

--- app/src/main/java/dev/motivateme/widget/QuoteWidgetReceiver.kt
+++ app/src/main/java/dev/motivateme/widget/QuoteWidgetReceiver.kt
@@ -1,4 +1,12 @@
 package dev.motivateme.widget
 
+import androidx.glance.appwidget.GlanceAppWidget
+import androidx.glance.appwidget.GlanceAppWidgetReceiver
+
 // Create the GlanceAppWidgetReceiver here named QuoteWidgetReceiver
 
+class QuoteWidgetReceiver : GlanceAppWidgetReceiver() {
+
+    override val glanceAppWidget: GlanceAppWidget =
+        QuoteWidget()
+}
