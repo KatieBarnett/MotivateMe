4a98289c9e72f36be3bb9bbf02cfd8fba9dfa8c5
Katie Barnett <katie.barnett@bilue.com.au>
2024-10-22 08:47:49 +1100

    Workshop: Activity 2, Task 3: Create a simple `GlanceAppWidget` that displays some text

--- app/src/main/java/dev/motivateme/widget/QuoteWidget.kt
+++ app/src/main/java/dev/motivateme/widget/QuoteWidget.kt
@@ -1,5 +1,62 @@
 package dev.motivateme.widget
 
+import android.content.Context
+import androidx.compose.runtime.Composable
+import androidx.glance.GlanceId
+import androidx.glance.GlanceModifier
+import androidx.glance.GlanceTheme
+import androidx.glance.appwidget.GlanceAppWidget
+import androidx.glance.appwidget.appWidgetBackground
+import androidx.glance.appwidget.provideContent
+import androidx.glance.layout.Alignment
+import androidx.glance.layout.Box
+import androidx.glance.layout.fillMaxSize
+import androidx.glance.preview.ExperimentalGlancePreviewApi
+import androidx.glance.preview.Preview
+import androidx.glance.text.Text
 
 // Create the GlanceAppWidget here named QuoteWidget
+
+class QuoteWidget : GlanceAppWidget() {
+
+    override suspend fun provideGlance(context: Context, id: GlanceId) {
+
+        // In this method, load data needed to render the AppWidget.
+        // Use `withContext` to switch to another thread for long
+        // running operations.
+
+        val displayText = "Hello widget!"
+
+        provideContent {
+            // UI code here
+            GlanceTheme {
+                QuoteWidgetContent(displayText)
+            }
+        }
+    }
+}
+
+@Composable
+fun QuoteWidgetContent(
+    displayText: String,
+    modifier: GlanceModifier = GlanceModifier
+) {
+    Box(
+        contentAlignment = Alignment.Center,
+        modifier = modifier
+            .fillMaxSize()
+            .appWidgetBackground(),
+    ) {
+        Text(displayText)
+    }
+}
+
+@OptIn(ExperimentalGlancePreviewApi::class)
+@Composable
+@Preview
+fun QuoteWidgetContentPreview() {
+    GlanceTheme {
+        QuoteWidgetContent("Hello widget!")
+    }
+}
 
