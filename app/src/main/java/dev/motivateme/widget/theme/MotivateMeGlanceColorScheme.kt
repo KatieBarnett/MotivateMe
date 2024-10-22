2a6acb2256af0a2e8d4b60988a8782dd1a5b9524
Katie Barnett <katiebarnett5@gmail.com>
2024-10-22 21:28:56 +1100

    Workshop: Activity 4, Task 2: Uncomment the code provided in `MotivateMeGlanceColorScheme`

--- app/src/main/java/dev/motivateme/widget/theme/MotivateMeGlanceColorScheme.kt
+++ app/src/main/java/dev/motivateme/widget/theme/MotivateMeGlanceColorScheme.kt
@@ -2,14 +2,14 @@ package dev.motivateme.widget.theme
 
 // Uncomment this for use in the GlanceTheme
 
-//import androidx.glance.material3.ColorProviders
-//import dev.motivateme.ui.theme.darkScheme
-//import dev.motivateme.ui.theme.lightScheme
+import androidx.glance.material3.ColorProviders
+import dev.motivateme.ui.theme.darkScheme
+import dev.motivateme.ui.theme.lightScheme
 
-//object MotivateMeGlanceColorScheme {
-//
-//    val colors = ColorProviders(
-//        light = lightScheme,
-//        dark = darkScheme
-//    )
-//}
+object MotivateMeGlanceColorScheme {
+
+    val colors = ColorProviders(
+        light = lightScheme,
+        dark = darkScheme
+    )
+}
