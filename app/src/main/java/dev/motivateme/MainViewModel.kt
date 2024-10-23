04f3f2e2a0a4d5d10cea6cc7eae0da83fc6aa8ed
Kartik Arora <kartik.arora1995@gmail.com>
2024-10-23 15:51:14 +1100

    Workshop: Activity 7, Task 2: Use Gemini to create a new topic & be able to generate quotes in the main app

--- app/src/main/java/dev/motivateme/MainViewModel.kt
+++ app/src/main/java/dev/motivateme/MainViewModel.kt
@@ -1,17 +1,28 @@
 package dev.motivateme
 
+import androidx.compose.runtime.getValue
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import dagger.hilt.android.lifecycle.HiltViewModel
 import dev.motivateme.data.DataRepository
+import dev.motivateme.data.GeminiInterface
+import dev.motivateme.models.Quote
+import dev.motivateme.models.Topic
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.SharingStarted
+import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.stateIn
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
 class MainViewModel @Inject constructor(
-    private val dataRepository: DataRepository
+    private val dataRepository: DataRepository,
+    private val geminiInterface: GeminiInterface
 ) : ViewModel() {
+
+    val showLoading = MutableStateFlow(true)
 
     val topics = dataRepository.getTopics().stateIn(
         viewModelScope,
@@ -19,7 +30,42 @@ class MainViewModel @Inject constructor(
         initialValue = listOf()
     )
 
-    fun getQuotes(topicName: String) =
-        topics.value.firstOrNull { it.name == topicName }?.quotes ?: listOf()
+    private val _generatedTopics = MutableStateFlow<List<Topic>>(emptyList())
+    val generatedTopics: StateFlow<List<Topic>> = _generatedTopics
 
+    private val _quotes = MutableStateFlow<List<Quote>>(emptyList())
+    val quotes: StateFlow<List<Quote>> = _quotes
+
+    init {
+        if (BuildConfig.IS_GEMINI_ENABLED) {
+            generateTopics()
+        }
+    }
+
+    fun getQuotes(topicName: String) {
+        val quotes = topics.value.firstOrNull { it.name == topicName }?.quotes ?: emptyList()
+        if (quotes.isEmpty()) {
+            generateQuotes(topicName)
+        } else {
+            _quotes.value = quotes
+        }
+    }
+
+    private fun generateTopics() {
+        viewModelScope.launch(Dispatchers.IO) {
+            showLoading.emit(true)
+            val newTopics = geminiInterface.generateTopics()
+            _generatedTopics.emit(newTopics)
+            showLoading.emit(false)
+        }
+    }
+
+    private fun generateQuotes(topicName: String) {
+        viewModelScope.launch(Dispatchers.IO) {
+            showLoading.emit(true)
+            val newQuotes = geminiInterface.getQuotes(topicName)
+            _quotes.emit(newQuotes)
+            showLoading.emit(false)
+        }
+    }
 }
---
+++ app/src/main/java/dev/motivateme/data/GeminiInterface.kt
@@ -0,0 +1,51 @@
+package dev.motivateme.data
+
+import com.google.ai.client.generativeai.GenerativeModel
+import dev.motivateme.BuildConfig
+import dev.motivateme.models.Quote
+import dev.motivateme.models.Topic
+import javax.inject.Inject
+import javax.inject.Singleton
+
+@Singleton
+class GeminiInterface @Inject constructor() {
+    private val apiKey get() = "YOUR_API_KEY"
+    private val splittingDelimiter = "::"
+
+    private val generativeModel by lazy {
+        if (BuildConfig.IS_GEMINI_ENABLED) {
+            GenerativeModel(
+                modelName = "gemini-1.5-flash-002",
+                apiKey = apiKey
+            )
+        } else {
+            null
+        }
+    }
+
+    suspend fun generateTopics(): List<Topic> {
+        val response =
+            generativeModel?.generateContent(
+                "Give 3 topics for writing quotes about. Make sure they are 2 words each, capitalised. Separate each of them by `$splittingDelimiter` only."
+            )
+        val topics = response?.text?.split(splittingDelimiter)?.map {
+            Topic(
+                name = it.trim(),
+                isGenerative = true,
+                quotes = emptyList()
+            )
+        } ?: emptyList()
+        return topics
+    }
+
+    suspend fun getQuotes(topicName: String): List<Quote> {
+        val response =
+            generativeModel?.generateContent(
+                "Give me quotes on the topic of $topicName. I need at only 3 quotes. Separate each quote by `$splittingDelimiter` only."
+            )
+        val quotes = response?.text?.split(splittingDelimiter)?.map {
+            Quote(text = it.trim())
+        } ?: emptyList()
+        return quotes
+    }
+}
