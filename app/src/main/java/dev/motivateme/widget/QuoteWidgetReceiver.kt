67f84362245362b71ac42ce9696ac5d2d92647f7
Kartik Arora <kartik.arora1995@gmail.com>
2024-10-25 08:21:40 +1100

    Workshop: Activity 9, Task 2: Update to `QuoteWidgetReceiver` to set up the periodic data updating with `QuoteWidgetWorker`

--- app/src/main/java/dev/motivateme/widget/QuoteWidgetReceiver.kt
+++ app/src/main/java/dev/motivateme/widget/QuoteWidgetReceiver.kt
@@ -1,5 +1,7 @@
 package dev.motivateme.widget
 
+import android.appwidget.AppWidgetManager
+import android.content.Context
 import androidx.glance.appwidget.GlanceAppWidget
 import androidx.glance.appwidget.GlanceAppWidgetReceiver
 
@@ -9,4 +11,18 @@ class QuoteWidgetReceiver : GlanceAppWidgetReceiver() {
 
     override val glanceAppWidget: GlanceAppWidget =
         QuoteWidget()
+
+    override fun onUpdate(
+        context: Context,
+        appWidgetManager: AppWidgetManager,
+        appWidgetIds: IntArray,
+    ) {
+        super.onUpdate(context, appWidgetManager, appWidgetIds)
+        QuoteWidgetWorker.enqueue(context)
+    }
+
+    override fun onDeleted(context: Context, appWidgetIds: IntArray) {
+        super.onDeleted(context, appWidgetIds)
+        QuoteWidgetWorker.cancel(context)
+    }
 }
